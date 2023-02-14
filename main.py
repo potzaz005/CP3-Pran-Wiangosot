13a10322bbc5118e028403d45f97b94dbccdf7ab
potzaz005 <taelerr@hotmail.com>
2023-02-14 19:03:10 +0700

    EX8

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+username = (input("username"))
+password = (input("password"))
+if username =="potae" and password =="1234":
+    print("Welcome")
+    print("1.Apple 10 THB")
+    print("2.Banana 5 THB")
+    UserSelected = int(input("What do you want"))
+    if UserSelected ==1:
+        Apple = int(input("unit"))
+        price = 10
+        result = Apple * price
+        print(result)
+    elif UserSelected ==2:
+        Banana = int(input("unit"))
+        price = 5
+        result = Banana * price
+        print(result)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
